c62d7bdfdf43bf98b9298b6660aea6d5ab66e485
Mike-U5 <maikel.haarmans2@gmail.com>
2023-01-22 17:01:54 +0100

    Add actions

--- src/main/java/com/loreandhistory/components/LoreButton.java
+++ src/main/java/com/loreandhistory/components/LoreButton.java
@@ -16,21 +16,31 @@ final public class LoreButton {
 		this.client = client;
 
 		this.widget = parent.createChild(-1, WidgetType.GRAPHIC);
-		this.widget.setPos(150, 0);
+		this.widget.setPos(205, 0);
 		this.widget.setSize(40, 40);
 		this.widget.setSpriteId(SpriteID.SPELL_NPC_CONTACT);
 
+		this.widget.setAction(0, "Play");
+		this.widget.setAction(1, "Pause");
+		this.widget.setAction(2, "Stop");
+
+		this.widget.setOnOpListener((JavaScriptCallback)this::onOptionSelected);
 		this.widget.setOnMouseOverListener((JavaScriptCallback)this::onMouseHover);
 		this.widget.setOnMouseLeaveListener((JavaScriptCallback)this::onMouseLeave);
 		this.widget.setHasListener(true);
 	}
 
-	private void onMouseHover(ScriptEvent e)
+	private void onOptionSelected(final ScriptEvent e)
+	{
+		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "CHOSE " + e.getOp(), null);
+	}
+
+	private void onMouseHover(final ScriptEvent e)
 	{
 		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "HOVER", null);
 	}
 
-	private void onMouseLeave(ScriptEvent e)
+	private void onMouseLeave(final ScriptEvent e)
 	{
 		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "LEAVE", null);
 	}
