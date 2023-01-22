e8eb8549662cd2aac2ec3b0e111c49d3ff0ef9e5
Mike-U5 <maikel.haarmans2@gmail.com>
2023-01-22 17:34:29 +0100

    Set icon

--- src/main/java/com/loreandhistory/components/LoreButton.java
+++ src/main/java/com/loreandhistory/components/LoreButton.java
@@ -3,12 +3,23 @@ package com.loreandhistory.components;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.ScriptEvent;
-import net.runelite.api.SpriteID;
 import net.runelite.api.widgets.JavaScriptCallback;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetType;
 
+//1706: Post
+//2149: Letter
+//2275: League Icon
+//2415: Newspaper
+//3018: Paper with question mark
+//3295: Clan Book Icon
+//3424: Yellow scroll
+//3431: Yellow book
+//3566: Scroll with question mark orange outline
+//4175: Book relic (4177)
+//4753: Play(ish) button
 final public class LoreButton {
+	private int spriteId = 1706;
 	private final Client client;
 	private final Widget widget;
 
@@ -18,7 +29,7 @@ final public class LoreButton {
 		this.widget = parent.createChild(-1, WidgetType.GRAPHIC);
 		this.widget.setPos(205, 0);
 		this.widget.setSize(40, 40);
-		this.widget.setSpriteId(SpriteID.SPELL_NPC_CONTACT);
+		this.widget.setSpriteId(2149);
 
 		this.widget.setAction(0, "Play");
 		this.widget.setAction(1, "Pause");
@@ -32,6 +43,14 @@ final public class LoreButton {
 
 	private void onOptionSelected(final ScriptEvent e)
 	{
+		if (e.getOp() == 1) {
+
+		} else if (e.getOp() == 2) {
+
+		} else if (e.getOp() == 3) {
+
+		}
+
 		client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "CHOSE " + e.getOp(), null);
 	}
 
